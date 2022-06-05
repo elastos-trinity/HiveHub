10d0c169497ce1c269cbc63d1dca43c461a9187c
blockchaindev91 <hisamitsuteru@gmail.com>
2022-06-06 02:05:06 +0900

    feat: add a mock explore page

---
+++ src/pages/Dashboard/Explore/index.js
@@ -0,0 +1,7 @@
+export default function HiveExplore() {
+  return (
+    <div>
+      <h1>Explore</h1>
+    </div>
+  );
+}
--- src/pages/Dashboard/MyVaults/index.js
+++ src/pages/Dashboard/MyVaults/index.js
@@ -1,7 +1,7 @@
 export default function HiveVaults() {
   return (
     <div>
-      <h1>HiveVaults</h1>
+      <h1>My Vaults</h1>
     </div>
   );
 }

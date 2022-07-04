97bcc50e2a8716aa4691d45f29365bfe233765d7
blockchaindev91 <hisamitsuteru@gmail.com>
2022-07-04 21:39:42 +0900

    fix: add form validation for creating hive node page

--- src/pages/Dashboard/MyNodes/CreateNode.js
+++ src/pages/Dashboard/MyNodes/CreateNode.js
@@ -1,8 +1,10 @@
-import * as React from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Box, Button, Stack, TextField } from '@mui/material';
-import { styled } from '@mui/material/styles';
+import { Box, Button, Stack } from '@mui/material';
+import { styled, useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import { PageTitleTypo } from '../style';
+import CustomTextField from '../../../components/CustomTextField';
 
 const ContainerBox = styled(Box)(({ theme }) => ({
   backgroundColor: '#fff',
@@ -13,16 +15,6 @@ const ContainerBox = styled(Box)(({ theme }) => ({
   padding: '20px 50px 20px 20px',
   [theme.breakpoints.up('md')]: {
     padding: '30px 70px'
-  }
-}));
-
-const CustomTextField = styled(TextField)(({ theme }) => ({
-  width: '100%',
-  fontSize: '10px',
-  lineHeight: '12px',
-  [theme.breakpoints.up('md')]: {
-    fontSize: '20px',
-    lineHeight: '24px'
   }
 }));
 
@@ -48,30 +40,149 @@ const CustomButton = styled(Button)(({ theme }) => ({
 
 export default function CreateNode() {
   const navigate = useNavigate();
+  const [ownerDid, setOwnerDid] = useState('');
+  const [ownerDidErr, setOwnerDidErr] = useState(false);
+  const [nodeName, setNodeName] = useState('');
+  const [nodeNameErr, setNodeNameErr] = useState(false);
+  const [email, setEmail] = useState('');
+  const [emailErr, setEmailErr] = useState(false);
+  const [country, setCountry] = useState('');
+  const [countryErr, setCountryErr] = useState(false);
+  const [province, setProvince] = useState('');
+  const [provinceErr, setProvinceErr] = useState(false);
+  const [district, setDistrict] = useState('');
+  const [districtErr, setDistrictErr] = useState(false);
+  const [url, setUrl] = useState('');
+  const [urlErr, setUrlErr] = useState(false);
+  const [description, setDescription] = useState('');
+  const [descriptionErr, setDescriptionErr] = useState(false);
 
   const handleCreateNode = () => {
-    navigate('/dashboard/nodes');
+    if (ownerDid && nodeName && email && country && province && district && url && description) {
+      alert(1);
+      navigate('/dashboard/nodes');
+    } else {
+      setOwnerDidErr(!ownerDid);
+      setNodeNameErr(!nodeName);
+      setEmailErr(!email);
+      setCountryErr(!country);
+      setProvinceErr(!province);
+      setDistrictErr(!district);
+      setUrlErr(!url);
+      setDescriptionErr(!description);
+    }
   };
+
+  const theme = useTheme();
+  const matchDownMd = useMediaQuery(theme.breakpoints.down('md'));
 
   return (
     <>
       <PageTitleTypo mt={{ xs: 6.25, md: 3.15 }}>Create Node</PageTitleTypo>
       <ContainerBox mt={{ xs: 2.5, md: 5 }}>
         <Stack spacing={{ xs: 5, md: 7.5 }} mt={{ xs: 3.75, md: 5 }}>
-          <CustomTextField placeholder="Owner DID" variant="standard" />
-          <CustomTextField placeholder="Node Name" variant="standard" />
-          <CustomTextField placeholder="Email" variant="standard" />
+          <CustomTextField
+            placeholder="Owner DID"
+            variant="standard"
+            fontSize={matchDownMd ? 10 : 20}
+            height={matchDownMd ? 12 : 24}
+            error={ownerDidErr}
+            errorText="Owner DID can not be empty"
+            changeHandler={(value) => {
+              setOwnerDid(value);
+              setOwnerDidErr(false);
+            }}
+          />
+          <CustomTextField
+            placeholder="Node Name"
+            variant="standard"
+            fontSize={matchDownMd ? 10 : 20}
+            height={matchDownMd ? 12 : 24}
+            error={nodeNameErr}
+            errorText="Node Name can not be empty"
+            changeHandler={(value) => {
+              setNodeName(value);
+              setNodeNameErr(false);
+            }}
+          />
+          <CustomTextField
+            placeholder="Email"
+            variant="standard"
+            fontSize={matchDownMd ? 10 : 20}
+            height={matchDownMd ? 12 : 24}
+            error={emailErr}
+            errorText="Email can not be empty"
+            changeHandler={(value) => {
+              setEmail(value);
+              setEmailErr(false);
+            }}
+          />
           <Stack
             direction={{ xs: 'column', md: 'row' }}
-            alignItems="center"
+            alignItems={{ xs: 'left', md: 'center' }}
             spacing={{ xs: 5, md: 12.5 }}
           >
-            <CustomTextField placeholder="Country" variant="standard" />
-            <CustomTextField placeholder="Province" variant="standard" />
-            <CustomTextField placeholder="District" variant="standard" />
+            <CustomTextField
+              placeholder="Country"
+              variant="standard"
+              fontSize={matchDownMd ? 10 : 20}
+              height={matchDownMd ? 12 : 24}
+              error={countryErr}
+              errorText="Country can not be empty"
+              changeHandler={(value) => {
+                setCountry(value);
+                setCountryErr(false);
+              }}
+            />
+            <CustomTextField
+              placeholder="Province"
+              variant="standard"
+              fontSize={matchDownMd ? 10 : 20}
+              height={matchDownMd ? 12 : 24}
+              error={provinceErr}
+              errorText="Province can not be empty"
+              changeHandler={(value) => {
+                setProvince(value);
+                setProvinceErr(false);
+              }}
+            />
+            <CustomTextField
+              placeholder="District"
+              variant="standard"
+              fontSize={matchDownMd ? 10 : 20}
+              height={matchDownMd ? 12 : 24}
+              error={districtErr}
+              errorText="District can not be empty"
+              changeHandler={(value) => {
+                setDistrict(value);
+                setDistrictErr(false);
+              }}
+            />
           </Stack>
-          <CustomTextField placeholder="URL" variant="standard" />
-          <CustomTextField placeholder="Description" variant="standard" />
+          <CustomTextField
+            placeholder="URL"
+            variant="standard"
+            fontSize={matchDownMd ? 10 : 20}
+            height={matchDownMd ? 12 : 24}
+            error={urlErr}
+            errorText="Url can not be empty"
+            changeHandler={(value) => {
+              setUrl(value);
+              setUrlErr(false);
+            }}
+          />
+          <CustomTextField
+            placeholder="Description"
+            variant="standard"
+            fontSize={matchDownMd ? 10 : 20}
+            height={matchDownMd ? 12 : 24}
+            error={descriptionErr}
+            errorText="Description can not be empty"
+            changeHandler={(value) => {
+              setDescription(value);
+              setDescriptionErr(false);
+            }}
+          />
         </Stack>
         <Stack direction="row" mt={{ xs: 10, md: 17.5 }} spacing={{ xs: 2.5, md: 5 }}>
           <CustomButton

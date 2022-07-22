0f3d8999a77eeaab8dbe06ea480d536e3d5bb4dc
blockchaindev91 <hisamitsuteru@gmail.com>
2022-07-22 10:13:48 +0900

    fix: remove unnecessary components and files

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,4 @@
 import { Navigate, useRoutes } from 'react-router-dom';
-// layouts
-// import DashboardLayout from './layouts/dashboard';
-// import LogoOnlyLayout from './layouts/LogoOnlyLayout';
-//
-// import Login from './pages/Login';
-// import Register from './pages/Register';
-// import DashboardApp from './pages/DashboardApp';
-// import Products from './pages/Products';
-// import Blog from './pages/Blog';
-// import User from './pages/User';
 // import NotFound from './pages/Page404';
 import LandingPage from './pages/Landing';
 import HiveMainPage from './layouts/Landing';

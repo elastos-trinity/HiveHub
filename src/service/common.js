59d0b978e993f7136e38830247ef3a2ba8467c4d
blockchaindev91 <hisamitsuteru@gmail.com>
2022-08-18 15:18:35 +0900

    fix: add a function to check the ip's validation

--- src/service/common.js
+++ src/service/common.js
@@ -48,3 +48,10 @@ export const sleep = (ms) =>
   new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
+
+export const checkIfValidIP = (str) => {
+  // Regular expression to check if string is a IP address
+  const regexExp =
+    /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gi;
+  return regexExp.test(str);
+};

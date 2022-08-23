8273fffe96e9c810bd909ba895f1a75f350f5cbe
blockchaindev91 <hisamitsuteru@gmail.com>
2022-08-23 18:31:46 +0900

    feat: add modal dialog component

---
+++ src/components/ModalDialog.js
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import { Dialog } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+ModalDialog.propTypes = {
+  open: PropTypes.bool,
+  onClose: PropTypes.func,
+  children: PropTypes.node
+};
+
+export default function ModalDialog({ open, onClose, children }) {
+  const theme = useTheme();
+  const matchDownMd = useMediaQuery(theme.breakpoints.down('md'));
+
+  return (
+    <Dialog
+      fullScreen={matchDownMd}
+      open={open}
+      onClose={onClose}
+      sx={{ background: '#1890FF90' }}
+      PaperProps={{
+        sx: {
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          maxWidth: '100%',
+          padding: { xs: 4, md: 8 },
+          borderRadius: { xs: 0, md: 5 }
+        }
+      }}
+    >
+      {children}
+    </Dialog>
+  );
+}

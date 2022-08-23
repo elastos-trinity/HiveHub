8d41df9291e6e0b61d9983812dedd40dce147da4
blockchaindev91 <hisamitsuteru@gmail.com>
2022-08-23 18:32:24 +0900

    feat: add dialog context

---
+++ src/contexts/DialogContext.js
@@ -0,0 +1,37 @@
+import { createContext, useState, useContext } from 'react';
+import PropTypes from 'prop-types';
+
+const initialState = {
+  dlgState: {},
+  setDlgState: () => {}
+};
+
+const DialogContext = createContext(initialState);
+
+DialogContextProvider.propTypes = {
+  children: PropTypes.node
+};
+
+function DialogContextProvider({ children }) {
+  const [dlgState, setDlgState] = useState({
+    confirmDlgOpened: false,
+    removeNodeNid: 0,
+    removeNodeOwnerDid: ''
+  });
+
+  return (
+    <DialogContext.Provider
+      // eslint-disable-next-line react/jsx-no-constructed-context-values
+      value={{
+        dlgState,
+        setDlgState
+      }}
+    >
+      {children}
+    </DialogContext.Provider>
+  );
+}
+
+const useDialogContext = () => useContext(DialogContext);
+
+export { DialogContextProvider, DialogContext, useDialogContext };

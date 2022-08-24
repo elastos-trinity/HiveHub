1337872586771b626df79f889d01a74184a28eba
blockchaindev91 <hisamitsuteru@gmail.com>
2022-08-24 22:55:59 +0900

    fix: add backup migrate button to my vaults page

--- src/pages/Dashboard/MyVaults/index.js
+++ src/pages/Dashboard/MyVaults/index.js
@@ -1,20 +1,27 @@
 import { useState, useEffect } from 'react';
-// import { useNavigate } from 'react-router-dom';
 import { Stack } from '@mui/material';
 import { useSnackbar } from 'notistack';
 import { PageTitleTypo } from '../../../components/CustomTypos';
 import VaultItem from '../../../components/VaultItem';
 import { emptyVaultItem } from '../../../utils/filler';
 import { useUserContext } from '../../../contexts/UserContext';
-import { createVault, getHiveVaultInfo } from '../../../service/fetch';
-import { PlusButton } from '../../../components/CustomButtons';
+import {
+  createVault,
+  getHiveVaultInfo,
+  backupVault,
+  checkBackupStatus,
+  findBackupNodeProvider,
+  migrateVault
+} from '../../../service/fetch';
+import { CustomButton, PlusButton } from '../../../components/CustomButtons';
 
 export default function HiveVaults() {
-  // const navigate = useNavigate();
   const { enqueueSnackbar } = useSnackbar();
   const { user } = useUserContext();
   const [loading, setLoading] = useState(false);
   const [myVaultsList, setMyVaultsList] = useState(Array(1).fill(emptyVaultItem));
+  const [onProgress, setOnProgress] = useState(false);
+  const [hasBackup, setHasBackup] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,6 +29,7 @@ export default function HiveVaults() {
       const vaultItem = await getHiveVaultInfo(user.did, undefined, 1);
       if (vaultItem) setMyVaultsList([vaultItem]);
       else setMyVaultsList([]);
+      setHasBackup(await checkBackupStatus(user.did));
       setLoading(false);
     };
     fetchData();
@@ -51,6 +59,81 @@ export default function HiveVaults() {
       });
   };
 
+  const handleBackup = async () => {
+    if (!user.did) return;
+    if (!myVaultsList.length) return;
+    setOnProgress(true);
+    try {
+      const backupNodeProvider = await findBackupNodeProvider(user.did);
+      console.log('Backup vault to: ', backupNodeProvider);
+      const result = await backupVault(user.did, backupNodeProvider);
+      if (result === 1) {
+        enqueueSnackbar('Backup vault succeed', {
+          variant: 'success',
+          anchorOrigin: { horizontal: 'right', vertical: 'top' }
+        });
+      } else if (result === 2) {
+        enqueueSnackbar('Already backup', {
+          variant: 'error',
+          anchorOrigin: { horizontal: 'right', vertical: 'top' }
+        });
+      } else {
+        enqueueSnackbar('Backup vault error', {
+          variant: 'error',
+          anchorOrigin: { horizontal: 'right', vertical: 'top' }
+        });
+      }
+    } catch (err) {
+      console.error(err);
+      enqueueSnackbar('Backup vault error', {
+        variant: 'error',
+        anchorOrigin: { horizontal: 'right', vertical: 'top' }
+      });
+    }
+    setOnProgress(false);
+  };
+
+  const handleMigrate = async () => {
+    if (!user.did) return;
+    if (!myVaultsList.length) return;
+    setOnProgress(true);
+    try {
+      const backupNodeProvider = await findBackupNodeProvider(user.did);
+      console.log('Migrate vault to: ', backupNodeProvider);
+      await migrateVault(user.did, backupNodeProvider);
+      enqueueSnackbar('Migrate vault succeed', {
+        variant: 'success',
+        anchorOrigin: { horizontal: 'right', vertical: 'top' }
+      });
+    } catch (err) {
+      console.log(err);
+      enqueueSnackbar('Migrate vault error', {
+        variant: 'error',
+        anchorOrigin: { horizontal: 'right', vertical: 'top' }
+      });
+    }
+  };
+
+  // For test
+  const handleUnbind = async () => {
+    if (!user.nodeProvider || user.did) return;
+    try {
+      console.log('Unbind DID from ', user.nodeProvider);
+      // await unbindDID(user.did);
+      enqueueSnackbar('Unbind DID succeed', {
+        variant: 'success',
+        anchorOrigin: { horizontal: 'right', vertical: 'top' }
+      });
+    } catch (err) {
+      console.log(err);
+      enqueueSnackbar('Unbind DID error', {
+        variant: 'error',
+        anchorOrigin: { horizontal: 'right', vertical: 'top' }
+      });
+    }
+    setOnProgress(false);
+  };
+
   return (
     <>
       <PageTitleTypo mt={{ xs: 5, md: 6 }} mb={myVaultsList.length ? 0 : 1.25}>
@@ -78,6 +161,27 @@ export default function HiveVaults() {
           <PlusButton onClick={() => {}} hasPlus={false} disabled>
             Access Hive Vaults
           </PlusButton>
+        </Stack>
+        <Stack
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          spacing={{ xs: 2, sm: 5 }}
+          px={1}
+          sx={{ width: '100%', margin: '40px auto' }}
+        >
+          <CustomButton
+            onClick={handleBackup}
+            disabled={!myVaultsList.length || hasBackup || onProgress}
+          >
+            Backup
+          </CustomButton>
+          <CustomButton onClick={handleMigrate} disabled={!myVaultsList.length || onProgress}>
+            Migrate
+          </CustomButton>
+          <CustomButton onClick={handleUnbind} disabled={!user.nodeProvider || onProgress || true}>
+            Unbind
+          </CustomButton>
         </Stack>
       </Stack>
     </>

52f9d05cf3ad2ba6f357bb79e604fc4bf281bd61
blockchaindev91 <hisamitsuteru@gmail.com>
2022-09-19 15:40:46 +0900

    feat: implement addHiveNodes  function

--- src/hooks/useHiveHubContracts.js
+++ src/hooks/useHiveHubContracts.js
@@ -3,6 +3,7 @@ import { useSnackbar } from 'notistack';
 import { isInAppBrowser } from '../service/common';
 import { essentialsConnector } from '../service/connectivity';
 import { callContractMethod } from '../service/contract';
+import { uploadNode2Ipfs } from '../service/ipfs';
 
 export default function useHiveHubContracts() {
   const { enqueueSnackbar } = useSnackbar;
@@ -12,21 +13,51 @@ export default function useHiveHubContracts() {
   const walletConnectWeb3 = new Web3(walletConnectProvider);
 
   const getHiveNodes = async () => {};
-  const addHiveNode = async (tokenId, tokenUri, nodeEntry) => {
-    const platformInfo = await callContractMethod(walletConnectWeb3, {
-      methodName: 'getPlatformFee',
-      callType: 'call',
-      price: '0'
-    });
-    await callContractMethod(walletConnectWeb3, {
-      methodName: 'mint',
-      callType: 'send',
-      price: platformInfo.platformFee,
-      tokenId,
-      tokenUri,
-      nodeEntry
-    });
+
+  const addHiveNode = async (nodeInfo) => {
+    try {
+      const tokenId = await callContractMethod(walletConnectWeb3, {
+        methodName: 'totalSupply',
+        callType: 'call',
+        price: '0'
+      });
+      const metaRes = await uploadNode2Ipfs(
+        nodeInfo.name,
+        nodeInfo.created,
+        nodeInfo.ip,
+        nodeInfo.owner_did,
+        nodeInfo.area,
+        nodeInfo.email,
+        nodeInfo.url,
+        nodeInfo.remark
+      );
+      const tokenUri = `hivehub:json:${metaRes.path}`;
+      const nodeEntry = nodeInfo.url;
+      const platformInfo = await callContractMethod(walletConnectWeb3, {
+        methodName: 'getPlatformFee',
+        callType: 'call',
+        price: '0'
+      });
+      console.log(
+        `Register Hive Node with tokenId: ${tokenId}, tokenUri: ${tokenUri}, nodeEntry: ${nodeEntry}, registerFee: ${
+          platformInfo.platformFee / 1e18
+        } ELA`
+      );
+      await callContractMethod(walletConnectWeb3, {
+        methodName: 'mint',
+        callType: 'send',
+        price: platformInfo.platformFee,
+        tokenId,
+        tokenUri,
+        nodeEntry
+      });
+      return true;
+    } catch (err) {
+      console.error(err);
+      return false;
+    }
   };
+
   const removeHiveNode = async (tokenId) => {
     try {
       await callContractMethod(walletConnectWeb3, {
@@ -43,7 +74,6 @@ export default function useHiveHubContracts() {
   };
 
   return {
-    callContractMethod,
     getHiveNodes,
     addHiveNode,
     removeHiveNode

212536f32d4d81ac31629ae3b430d4e02f2ccb1c
blockchaindev91 <hisamitsuteru@gmail.com>
2022-09-19 09:46:14 +0900

    feat: upload node to get metadata

---
+++ src/service/ipfs.js
@@ -0,0 +1,30 @@
+// eslint-disable-next-line import/no-unresolved
+import { create } from 'ipfs-http-client';
+import { config } from '../config';
+
+const client = create({ url: config.IPFSUploadUrl });
+
+export const uploadNode2Ipfs = (name, created, ip, ownerDid, area, email, url, remark) =>
+  new Promise((resolve, reject) => {
+    // create the metadata object we'll be storing
+    const metaObj = {
+      version: '1',
+      name,
+      created,
+      ip,
+      owner_did: ownerDid,
+      area,
+      email,
+      url,
+      remark
+    };
+    try {
+      const jsonMetaObj = JSON.stringify(metaObj);
+      // add the metadata itself as well
+      const metaRecv = Promise.resolve(client.add(jsonMetaObj));
+      const nodeUri = `hivehub:json:${metaRecv.path}`;
+      resolve(nodeUri);
+    } catch (error) {
+      reject(error);
+    }
+  });

51f557fa9b77cca418c1ee27acd951099c5a9ce2
hteru91 <hisamitsuteru@gmail.com>
2022-11-24 16:26:19 +0900

    feat: add skeleton loading to node detail page

--- src/pages/Dashboard/Node/NodeDetail.js
+++ src/pages/Dashboard/Node/NodeDetail.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useNavigate, useParams } from 'react-router-dom';
-import { Box, Chip, Stack, Avatar } from '@mui/material';
+import { Box, Chip, Stack, Avatar, Skeleton, Typography } from '@mui/material';
 import { NodeTitle, HeaderTypo, NormalTypo } from '../../../components/Custom/CustomTypos';
 import { ContainerBox } from '../../../components/Custom/CustomContainer';
 import { ConfirmButton } from '../../../components/Custom/CustomButtons';
@@ -42,7 +42,7 @@ export default function MyNodeDetail() {
   const { user } = useUserContext();
   const { getHiveNodeItem } = useHiveHubContracts();
   const { nodeId } = useParams();
-  const [isloading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [nodeDetail, setNodeDetail] = useState({});
 
   useEffect(() => {
@@ -82,60 +82,116 @@ export default function MyNodeDetail() {
           />
         </Stack>
       </Box>
-      <ContainerBox sx={{ position: 'relative', borderRadius: '0px 0px 20px 20px' }}>
-        <Avatar
-          src={nodeDetail?.avatar || ''}
-          alt="avatarURL"
-          sx={{
-            position: 'absolute',
-            width: { xs: '50px', md: '100px' },
-            height: { xs: '50px', md: '100px' },
-            top: { xs: '-25px', md: '-50px' },
-            left: 0,
-            right: 0,
-            margin: 'auto'
-          }}
-        />
-        <Stack
-          direction="row"
-          spacing={2.5}
-          alignItems="center"
-          sx={{ mt: { xs: 2.5, md: 5 }, mx: 'auto', width: 'fit-content' }}
-        >
-          <NodeTitle>{nodeDetail?.name || '???'}</NodeTitle>
-          {nodeDetail?.status ? (
-            <Chip
-              label="online"
-              color="success"
-              sx={{
-                height: { xs: '11px !important', md: '19px !important' },
-                color: '#FFFFFF',
-                '& .MuiChip-label': {
-                  px: { xs: '5px !important', md: '12px !important' }
-                }
-              }}
-            />
-          ) : (
-            <Chip
-              label="offline"
-              color="error"
-              sx={{
-                height: { xs: '11px !important', md: '19px !important' },
-                color: '#FFFFFF',
-                '& .MuiChip-label': {
-                  px: { xs: '5px !important', md: '12px !important' }
-                }
-              }}
-            />
-          )}
-        </Stack>
-        <NormalTypo sx={{ color: '#B3B3B3', py: 1 }}>{nodeDetail?.description || ''}</NormalTypo>
-        <Stack spacing={{ xs: 1, md: 2.5 }} mt={{ xs: 2, md: 7 }} mb={{ xs: 1, md: 4 }}>
-          {detailInfo.map((item, index) => (
-            <DetailItem key={index} label={item.label} value={nodeDetail[item.field] || '---'} />
-          ))}
-        </Stack>
-      </ContainerBox>
+      {isLoading ? (
+        <ContainerBox sx={{ position: 'relative', borderRadius: '0px 0px 20px 20px' }}>
+          {/* <Skeleton
+            variant="circular"
+            animation="wave"
+            width={{ xs: '50px', md: '100px' }}
+            height={{ xs: '50px', md: '100px' }}
+            sx={{
+              // bgcolor: 'grey',
+              position: 'absolute',
+              width: { xs: '50px', md: '100px' },
+              height: { xs: '50px', md: '100px' },
+              top: { xs: '-25px', md: '-50px' },
+              left: 0,
+              right: 0,
+              margin: 'auto'
+            }}
+          /> */}
+          <Avatar
+            src={nodeDetail?.avatar || ''}
+            alt="avatarURL"
+            sx={{
+              position: 'absolute',
+              width: { xs: '50px', md: '100px' },
+              height: { xs: '50px', md: '100px' },
+              top: { xs: '-25px', md: '-50px' },
+              left: 0,
+              right: 0,
+              margin: 'auto'
+            }}
+          />
+          <Typography
+            component="div"
+            variant="h4"
+            sx={{ mt: { xs: 2.5, md: 5 }, mx: 'auto', width: { xs: '100%', md: '40%' } }}
+          >
+            <Skeleton animation="wave" />
+          </Typography>
+          <Typography component="div" variant="h6">
+            <Skeleton animation="wave" />
+          </Typography>
+          <Stack spacing={{ xs: 1, md: 2.5 }} mt={{ xs: 2, md: 7 }} mb={{ xs: 1, md: 4 }}>
+            {detailInfo.map((_, index) => (
+              <Stack key={index} direction="row" spacing={{ xs: '5px', md: '10px' }}>
+                <Typography component="div" variant="h6" sx={{ width: '140px' }}>
+                  <Skeleton animation="wave" />
+                </Typography>
+                <Typography component="div" variant="h6" sx={{ width: '100%' }}>
+                  <Skeleton animation="wave" />
+                </Typography>
+              </Stack>
+            ))}
+          </Stack>
+        </ContainerBox>
+      ) : (
+        <ContainerBox sx={{ position: 'relative', borderRadius: '0px 0px 20px 20px' }}>
+          <Avatar
+            src={nodeDetail?.avatar || ''}
+            alt="avatarURL"
+            sx={{
+              position: 'absolute',
+              width: { xs: '50px', md: '100px' },
+              height: { xs: '50px', md: '100px' },
+              top: { xs: '-25px', md: '-50px' },
+              left: 0,
+              right: 0,
+              margin: 'auto'
+            }}
+          />
+          <Stack
+            direction="row"
+            spacing={2.5}
+            alignItems="center"
+            sx={{ mt: { xs: 2.5, md: 5 }, mx: 'auto', width: 'fit-content' }}
+          >
+            <NodeTitle>{nodeDetail?.name || '???'}</NodeTitle>
+            {nodeDetail?.status ? (
+              <Chip
+                label="online"
+                color="success"
+                sx={{
+                  height: { xs: '11px !important', md: '19px !important' },
+                  color: '#FFFFFF',
+                  '& .MuiChip-label': {
+                    px: { xs: '5px !important', md: '12px !important' }
+                  }
+                }}
+              />
+            ) : (
+              <Chip
+                label="offline"
+                color="error"
+                sx={{
+                  height: { xs: '11px !important', md: '19px !important' },
+                  color: '#FFFFFF',
+                  '& .MuiChip-label': {
+                    px: { xs: '5px !important', md: '12px !important' }
+                  }
+                }}
+              />
+            )}
+          </Stack>
+          <NormalTypo sx={{ color: '#B3B3B3', py: 1 }}>{nodeDetail?.description || ''}</NormalTypo>
+          <Stack spacing={{ xs: 1, md: 2.5 }} mt={{ xs: 2, md: 7 }} mb={{ xs: 1, md: 4 }}>
+            {detailInfo.map((item, index) => (
+              <DetailItem key={index} label={item.label} value={nodeDetail[item.field] || '---'} />
+            ))}
+          </Stack>
+        </ContainerBox>
+      )}
       <ConfirmButton
         onClick={() => navigate('/dashboard/node')}
         sx={{

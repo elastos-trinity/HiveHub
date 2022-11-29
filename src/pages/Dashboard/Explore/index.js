a2641683295aee64dfedf9d16d061eea04ddde8d
hteru91 <hisamitsuteru@gmail.com>
2022-11-29 15:21:35 +0900

    feat: add ViewToggleGroup component

--- src/pages/Dashboard/Explore/index.js
+++ src/pages/Dashboard/Explore/index.js
@@ -1,9 +1,8 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Stack, ToggleButtonGroup, ToggleButton } from '@mui/material';
-import LanguageIcon from '@mui/icons-material/Language';
-import TableRowsIcon from '@mui/icons-material/TableRows';
-import StatusSelect from '../../../components/StatusSelect';
+import { Stack, Box } from '@mui/material';
+import StatusSelect from '../../../components/Explore/StatusSelect';
+import ViewToggleGroup from '../../../components/Explore/ViewToggleGroup';
 import EmptyNodeView from '../../../components/Explore/EmptyNodeView';
 import NodesMapView from '../../../components/Explore/NodesMapView';
 import NodeItemBox from '../../../components/NodeItemBox';
@@ -42,41 +41,12 @@ export default function ExploreNode() {
       <Stack direction="row" justifyContent="space-between">
         <HeaderTypo sx={{ py: 1 }}>Explore all public Hive nodes</HeaderTypo>
         <Stack direction="row" spacing={2}>
-          <StatusSelect selected={statusFilter} onChange={setStatusFilter} />
-          <ToggleButtonGroup
-            value={viewMode}
-            onChange={(_, value) => setViewMode(value)}
-            size="small"
-            color="warning"
-            exclusive
-          >
-            <ToggleButton
-              value={0}
-              sx={{
-                display: 'flex',
-                alignItems: 'center',
-                background: '#1D1F21',
-                border: '1px solid #FF931E',
-                borderRadius: '20px',
-                height: '55px'
-              }}
-            >
-              <LanguageIcon />
-            </ToggleButton>
-            <ToggleButton
-              value={1}
-              sx={{
-                display: 'flex',
-                alignItems: 'center',
-                background: '#1D1F21',
-                border: '1px solid #FF931E',
-                borderRadius: '20px',
-                height: '55px'
-              }}
-            >
-              <TableRowsIcon />
-            </ToggleButton>
-          </ToggleButtonGroup>
+          <Box>
+            <StatusSelect selected={statusFilter} onChange={setStatusFilter} />
+          </Box>
+          <Box>
+            <ViewToggleGroup selected={viewMode} onChange={setViewMode} />
+          </Box>
         </Stack>
       </Stack>
       {!isLoading && !nodeList.length && (
@@ -91,27 +61,33 @@ export default function ExploreNode() {
           <EmptyNodeView />
         </div>
       )}
-      {!!nodeList.length && (
-        <div>
-          {viewMode === 0 ? (
-            <NodesMapView nodes={nodeList} sx={{ mt: { xs: 2.5, md: 5 }, mb: 5 }} />
-          ) : (
-            <Stack mt={{ xs: 2.5, md: 5 }} mb={5} spacing={{ xs: 3.75, md: 6.25 }}>
-              {nodeList.map((item, index) => (
-                <NodeItemBox
-                  key={`MyNode-Item-${index}`}
-                  nodeId={item?.nid}
-                  name={item?.name}
-                  status={item?.status}
-                  time={item?.created}
-                  description={item?.remark}
-                  endpoint={item?.url}
-                  isLoading={isLoading}
-                />
-              ))}
-            </Stack>
-          )}
+      {!!nodeList.length && viewMode === 0 && (
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '80%'
+          }}
+        >
+          <NodesMapView nodes={nodeList} sx={{ mt: { xs: 2.5, md: 5 } }} />
         </div>
+      )}
+      {!!nodeList.length && viewMode === 1 && (
+        <Stack mt={{ xs: 2.5, md: 5 }} mb={5} spacing={{ xs: 3.75, md: 6.25 }}>
+          {nodeList.map((item, index) => (
+            <NodeItemBox
+              key={`MyNode-Item-${index}`}
+              nodeId={item?.nid}
+              name={item?.name}
+              status={item?.status}
+              time={item?.created}
+              description={item?.remark}
+              endpoint={item?.url}
+              isLoading={isLoading}
+            />
+          ))}
+        </Stack>
       )}
     </>
   );

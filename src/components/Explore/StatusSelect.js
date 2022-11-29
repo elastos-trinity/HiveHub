b6455832a1ade1b6233f900125465a6ab52f4c81
hteru91 <hisamitsuteru@gmail.com>
2022-11-29 15:14:16 +0900

    fix: update status select component

---
+++ src/components/Explore/StatusSelect.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Select } from '@mui/material';
+import MenuItem from '@mui/material/MenuItem';
+import PropTypes from 'prop-types';
+import { LabelTypo } from '../Custom/CustomTypos';
+
+const MenuProps = {
+  anchorOrigin: {
+    vertical: 'bottom',
+    horizontal: 'left'
+  },
+  transformOrigin: {
+    vertical: 'top',
+    horizontal: 'left'
+  },
+  variant: 'menu'
+};
+
+const menuItems = [
+  { name: 'All', status: undefined },
+  { name: 'Online', status: true },
+  { name: 'Offline', status: false }
+];
+
+StatusSelect.propTypes = {
+  selected: PropTypes.number,
+  onChange: PropTypes.func,
+  sx: PropTypes.object
+};
+
+export default function StatusSelect({ selected, onChange, sx = {} }) {
+  const handleChange = (event) => {
+    onChange(event.target.value);
+  };
+
+  return (
+    <Select
+      value={selected}
+      onChange={handleChange}
+      size="small"
+      inputProps={{
+        'aria-label': 'Without label'
+      }}
+      sx={{
+        background: '#1D1F21',
+        border: '1px solid #FF931E',
+        borderRadius: '20px',
+        width: { xs: '100%', md: '90px' },
+        '& .MuiSelect-select': {
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        },
+        '& .MuiOutlinedInput-notchedOutline': {
+          borderWidth: '0 !important'
+        },
+        ...sx
+      }}
+      MenuProps={{
+        ...MenuProps,
+        sx: {
+          '& .MuiMenu-list': {
+            background: '#131317',
+            '& li': {
+              justifyContent: 'left'
+            }
+          }
+        }
+      }}
+    >
+      {menuItems.map((item, i) => (
+        <MenuItem key={i} value={i} autoFocus={selected === i}>
+          <LabelTypo sx={{ fontFamily: 'Roboto', color: '#FFF' }}>{item.name}</LabelTypo>
+        </MenuItem>
+      ))}
+    </Select>
+  );
+}

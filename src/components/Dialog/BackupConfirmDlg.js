c6932763c4fa3aaccf375cd5f14c975a686ac348
hteru91 <hisamitsuteru@gmail.com>
2022-12-06 17:00:09 +0900

    feat: add backup confirmation dlg

---
+++ src/components/Dialog/BackupConfirmDlg.js
@@ -0,0 +1,84 @@
+import PropTypes from 'prop-types';
+import { Stack } from '@mui/material';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { useTranslation } from 'react-i18next';
+import { HeaderTypo, NormalTypo } from '../Custom/CustomTypos';
+import { ConfirmButton } from '../Custom/CustomButtons';
+
+BackupConfirmDlg.propTypes = {
+  open: PropTypes.bool,
+  onClose: PropTypes.func,
+  onClick: PropTypes.func,
+  disabled: PropTypes.bool
+};
+
+export default function BackupConfirmDlg({ open, onClose, onClick, disabled }) {
+  const { t } = useTranslation();
+  return (
+    <Dialog
+      open={open}
+      aria-labelledby="backup-dialog-title"
+      aria-describedby="backup-dialog-description"
+      PaperProps={{
+        sx: {
+          background: '#161C24',
+          borderRadius: '40px',
+          px: 1,
+          py: 2
+        }
+      }}
+      TransitionProps={{
+        sx: {
+          background: 'rgba(22, 28, 36, 0.5)',
+          backdropFilter: 'blur(10px)',
+          borderRadius: '0px'
+        }
+      }}
+    >
+      <DialogTitle id="backup-dialog-title">
+        <HeaderTypo sx={{ py: 1, textAlign: 'center' }}>{t('btn-backup')}</HeaderTypo>
+      </DialogTitle>
+      <DialogContent>
+        <img
+          src="/static/ic_backup.svg"
+          alt="backup_icon"
+          width="60px"
+          style={{ margin: '0 auto 40px auto' }}
+        />
+        <DialogContentText id="backup-dialog-description">
+          <NormalTypo sx={{ py: 1, px: { xs: 1, md: 2 }, textAlign: 'center' }}>
+            {t('dlg-backup-confirm-label')}
+          </NormalTypo>
+        </DialogContentText>
+        <Stack
+          direction="row"
+          mt={{ xs: 8, md: 10 }}
+          spacing={{ xs: 1.5, md: 3 }}
+          justifyContent="center"
+        >
+          <ConfirmButton
+            onClick={onClose}
+            sx={{
+              color: '#FF931E',
+              background: 'transparent',
+              border: { xs: '1px solid #FF931E', md: '2px solid #FF931E' },
+              width: { xs: '120px', md: '240px' }
+            }}
+          >
+            {t('btn-cancel')}
+          </ConfirmButton>
+          <ConfirmButton
+            onClick={onClick}
+            sx={{ width: { xs: '120px', md: '240px' } }}
+            disabled={disabled}
+          >
+            {t('btn-backup')}
+          </ConfirmButton>
+        </Stack>
+      </DialogContent>
+    </Dialog>
+  );
+}

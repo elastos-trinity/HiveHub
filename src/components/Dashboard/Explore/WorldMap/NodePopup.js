2ed6e83fdab36fd7ffdf6d1faa0055b23697dac7
hteru91 <hisamitsuteru@gmail.com>
2022-12-15 17:08:26 +0900

    fix: add scroll bar to NodePopup

--- src/components/Dashboard/Explore/WorldMap/NodePopup.js
+++ src/components/Dashboard/Explore/WorldMap/NodePopup.js
@@ -4,6 +4,7 @@ import { Box, Stack, Typography, Chip, Popper, Fade, Button } from '@mui/materia
 import { useTranslation } from 'react-i18next';
 import { BaseTypography } from '../../../Custom/CustomTypos';
 import { getMapX, getMapY, getHexFromCircle } from './utils';
+import Scrollbar from '../../../Scrollbar';
 
 NodePopup.propTypes = {
   data: PropTypes.any,
@@ -70,161 +71,179 @@ export default function NodePopup({ data = [], isLoading, onClick, sx }) {
                 backgroundColor: '#131317',
                 borderRadius: '20px',
                 width: '100%',
+                height: '300px',
+                overflow: 'auto',
                 padding: '10px 10px 10px 20px',
                 ...sx
               }}
             >
-              {data.map((item, index) => {
-                const {
-                  name,
-                  created: time,
-                  remark: description,
-                  ip: endpoint,
-                  owner_did: ownerDid,
-                  status,
-                  nid
-                } = item;
-                return (
-                  <Box key={index} sx={{ position: 'relative' }}>
-                    <BaseTypography
-                      sx={{
-                        color: '#B3B3B3',
-                        fontSize: '10px',
-                        lineHeight: '12px',
-                        fontWeight: 400,
-                        whiteSpace: 'nowrap',
-                        position: 'absolute',
-                        right: '10px',
-                        top: '10px'
-                      }}
-                    >
-                      {time}
-                    </BaseTypography>
-                    <Stack spacing="20px">
-                      <Stack spacing="10px" py="10px">
-                        <Stack direction="row" alignItems="center" spacing="10px">
+              <Scrollbar
+                sx={{
+                  height: '100%',
+                  '& .simplebar-content': {
+                    height: '100%',
+                    display: 'flex',
+                    flexDirection: 'column'
+                  }
+                }}
+              >
+                {data.map((item, index) => {
+                  const {
+                    name,
+                    created: time,
+                    remark: description,
+                    ip: endpoint,
+                    owner_did: ownerDid,
+                    status,
+                    nid
+                  } = item;
+                  return (
+                    <Box key={index} sx={{ position: 'relative' }}>
+                      <BaseTypography
+                        sx={{
+                          color: '#B3B3B3',
+                          fontSize: '10px',
+                          lineHeight: '12px',
+                          fontWeight: 400,
+                          whiteSpace: 'nowrap',
+                          position: 'absolute',
+                          right: '10px',
+                          top: '10px'
+                        }}
+                      >
+                        {time}
+                      </BaseTypography>
+                      <Stack spacing="20px">
+                        <Stack spacing="10px" py="10px">
+                          <Stack direction="row" alignItems="center" spacing="10px">
+                            <BaseTypography
+                              sx={{
+                                fontWeight: 700,
+                                fontSize: '20px',
+                                lineHeight: '24px',
+                                color: '#FFF'
+                              }}
+                            >
+                              {name}
+                            </BaseTypography>
+                            {status ? (
+                              <Chip
+                                label={t('badge-online')}
+                                color="success"
+                                sx={{
+                                  height: '11px !important',
+                                  color: '#FFFFFF',
+                                  '& .MuiChip-label': {
+                                    px: '5px !important'
+                                  }
+                                }}
+                              />
+                            ) : (
+                              <Chip
+                                label={t('badge-offline')}
+                                color="error"
+                                sx={{
+                                  height: '11px !important',
+                                  color: '#FFFFFF',
+                                  '& .MuiChip-label': {
+                                    px: '5px !important'
+                                  }
+                                }}
+                              />
+                            )}
+                          </Stack>
                           <BaseTypography
                             sx={{
-                              fontWeight: 700,
-                              fontSize: '20px',
-                              lineHeight: '24px',
-                              color: '#FFF'
+                              fontWeight: 400,
+                              fontSize: '10px',
+                              lineHeight: '12px',
+                              color: '#B3B3B3'
                             }}
                           >
-                            {name}
+                            {description}
                           </BaseTypography>
-                          {status ? (
-                            <Chip
-                              label={t('badge-online')}
-                              color="success"
-                              sx={{
-                                height: '11px !important',
-                                color: '#FFFFFF',
-                                '& .MuiChip-label': {
-                                  px: '5px !important'
-                                }
-                              }}
-                            />
-                          ) : (
-                            <Chip
-                              label={t('badge-offline')}
-                              color="error"
-                              sx={{
-                                height: '11px !important',
-                                color: '#FFFFFF',
-                                '& .MuiChip-label': {
-                                  px: '5px !important'
-                                }
-                              }}
-                            />
-                          )}
                         </Stack>
-                        <BaseTypography
-                          sx={{
-                            fontWeight: 400,
-                            fontSize: '10px',
-                            lineHeight: '12px',
-                            color: '#B3B3B3'
-                          }}
-                        >
-                          {description}
-                        </BaseTypography>
-                      </Stack>
-                      <Stack direction="row" sx={{ display: 'flex', alignItems: 'end' }}>
-                        <Typography component="div" noWrap sx={{ flexGrow: 1 }} alignItems="center">
-                          <Stack spacing={1}>
-                            <Stack direction="row" sx={{ pb: '5px' }}>
-                              <BaseTypography
-                                sx={{
-                                  fontWeight: 400,
-                                  fontSize: '10px',
-                                  lineHeight: '12px',
-                                  color: '#B3B3B3',
-                                  pr: '5px'
-                                }}
-                              >
-                                {t('node-detail-endpoint')}:
-                              </BaseTypography>
-                              <BaseTypography
-                                sx={{
-                                  fontWeight: 600,
-                                  fontSize: '10px',
-                                  lineHeight: '12px',
-                                  color: '#FFF'
-                                }}
-                              >
-                                {endpoint}
-                              </BaseTypography>
+                        <Stack direction="row" sx={{ display: 'flex', alignItems: 'end' }}>
+                          <Typography
+                            component="div"
+                            noWrap
+                            sx={{ flexGrow: 1 }}
+                            alignItems="center"
+                          >
+                            <Stack spacing={1}>
+                              <Stack direction="row" sx={{ pb: '5px' }}>
+                                <BaseTypography
+                                  sx={{
+                                    fontWeight: 400,
+                                    fontSize: '10px',
+                                    lineHeight: '12px',
+                                    color: '#B3B3B3',
+                                    pr: '5px'
+                                  }}
+                                >
+                                  {t('node-detail-endpoint')}:
+                                </BaseTypography>
+                                <BaseTypography
+                                  sx={{
+                                    fontWeight: 600,
+                                    fontSize: '10px',
+                                    lineHeight: '12px',
+                                    color: '#FFF'
+                                  }}
+                                >
+                                  {endpoint}
+                                </BaseTypography>
+                              </Stack>
+                              <Stack direction="row" sx={{ pb: '5px' }}>
+                                <BaseTypography
+                                  sx={{
+                                    fontWeight: 400,
+                                    fontSize: '10px',
+                                    lineHeight: '12px',
+                                    color: '#B3B3B3',
+                                    pr: '5px'
+                                  }}
+                                >
+                                  {t('node-detail-owner-did')}:
+                                </BaseTypography>
+                                <BaseTypography
+                                  sx={{
+                                    fontWeight: 600,
+                                    fontSize: '10px',
+                                    lineHeight: '12px',
+                                    color: '#FFF'
+                                  }}
+                                  noWrap
+                                >
+                                  {ownerDid}
+                                </BaseTypography>
+                              </Stack>
                             </Stack>
-                            <Stack direction="row" sx={{ pb: '5px' }}>
-                              <BaseTypography
-                                sx={{
-                                  fontWeight: 400,
-                                  fontSize: '10px',
-                                  lineHeight: '12px',
-                                  color: '#B3B3B3',
-                                  pr: '5px'
-                                }}
-                              >
-                                {t('node-detail-owner-did')}:
-                              </BaseTypography>
-                              <BaseTypography
-                                sx={{
-                                  fontWeight: 600,
-                                  fontSize: '10px',
-                                  lineHeight: '12px',
-                                  color: '#FFF'
-                                }}
-                                noWrap
-                              >
-                                {ownerDid}
-                              </BaseTypography>
-                            </Stack>
-                          </Stack>
-                        </Typography>
-                        <Button
-                          sx={{
-                            mt: '10px',
-                            background: 'linear-gradient(270deg, #FF8A00 0%, #E23A45 100%)',
-                            borderRadius: '200px',
-                            color: 'white',
-                            fontWeight: 600,
-                            lineHeight: '12px',
-                            fontSize: '10px',
-                            height: '30px',
-                            padding: '7px 14px',
-                            width: 'fit-content',
-                            textTransform: 'inherit'
-                          }}
-                          onClick={() => onClick(nid)}
-                        >
-                          {t('btn-access')}
-                        </Button>
+                          </Typography>
+                          <Button
+                            sx={{
+                              mt: '10px',
+                              background: 'linear-gradient(270deg, #FF8A00 0%, #E23A45 100%)',
+                              borderRadius: '200px',
+                              color: 'white',
+                              fontWeight: 600,
+                              lineHeight: '12px',
+                              fontSize: '10px',
+                              height: '30px',
+                              padding: '7px 14px',
+                              width: 'fit-content',
+                              textTransform: 'inherit'
+                            }}
+                            onClick={() => onClick(nid)}
+                          >
+                            {t('btn-access')}
+                          </Button>
+                        </Stack>
                       </Stack>
-                    </Stack>
-                  </Box>
-                );
-              })}
+                    </Box>
+                  );
+                })}
+              </Scrollbar>
             </Stack>
           </Fade>
         )}
